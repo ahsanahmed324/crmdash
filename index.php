1edb03628912b78ae6d4073cd3e7d2cf1710b86d
ahsanahmed324 <158136626+ahsanahmed324@users.noreply.github.com>
2024-04-15 16:03:12 +0500

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ require __DIR__ . '/vendor/autoload.php';
 use Automattic\WooCommerce\Client;
 use Automattic\WooCommerce\HttpClient\HttpClientException;
 $woocommerce = new Client(
-    'https://tallynine.com', // Your store URL
-    'ck_bfc96e5f26d24ea8a617ce942da2790e62083e48', // Your consumer key
-    'cs_a3f52cce66b70ef398e84bfb799d587da873ddd3', // Your consumer secret
+    'https://70f5f7897a.nxcli.io/', // Your store URL
+    'ck_c38e44917a8a4be6ae423623655879d1a3ccafbf', // Your consumer key
+    'cs_31d43cda312ea0f137338b5699e12cd8f849fdda', // Your consumer secret
     [
         'wp_api' => true, // Enable the WP REST API integration
         'version' => 'wc/v2' // WooCommerce WP REST API version
