7773b8b33448fca295e431e2452d8697a2bb65bf
ahsanahmed324 <158136626+ahsanahmed324@users.noreply.github.com>
2024-04-19 15:00:20 +0500

    Update index.php

--- index.php
+++ index.php
@@ -158,7 +158,7 @@ if (isset($_POST['btn-delete'])) {
                           <td>" . $details["date_created"]."</td>
                           <td>" . $details["status"]."</td>
                           <td><a class='open-AddBookDialog btn btn-primary' data-target='#myModal' data-id=".$details['id']." data-toggle='modal'>Update</a>
-                          <a class='open-deleteDialog btn btn-danger' href='https://70f5f7897a.nxcli.io/'>View</a>
+                          <a class='open-deleteDialog btn btn-danger' href='singleview.php'>View</a>
                           <a class='open-deleteDialog btn btn-danger' data-target='#myModal1' data-id=".$details['id']." data-toggle='modal'>Delete</a></td></tr>";
                 }
                 ?>
