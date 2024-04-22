6f30e18ef5aade6fa96ae8dbfe0da2c7096e636c
ahsanahmed324 <158136626+ahsanahmed324@users.noreply.github.com>
2024-04-22 15:55:50 +0500

    Update index.php

--- index.php
+++ index.php
@@ -219,7 +219,7 @@ if (isset($_POST['btn-delete'])) {
                     foreach($products as $product){
 			
                     echo "<tr><td>" . $product["sku"].'</td>
-                              <td><a href="$product.["name"]">' . $product.["name"]."</a></td>
+                              <td><a href="'.$product.["name"].'">' . $product.["name"]."</a></td>
                               <td>" . $product["status"]."</td>
                               <td>" . $product["price"]."</td>
                               <td>" . $product["total_sales"]."</td>
