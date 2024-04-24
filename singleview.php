73dd4786e9770c35ed2cc6f01a4e46af268ddd24
ahsanahmed324 <158136626+ahsanahmed324@users.noreply.github.com>
2024-04-24 17:35:46 +0500

    Update singleview.php

--- singleview.php
+++ singleview.php
@@ -25,3 +25,44 @@
     print_r($ord);
     print "</pre>";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="style.css" />
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <title>Dashboard Template for Bootstrap</title>
+
+</head>
+             <div class="container">
+                              <h2 class="sub-header">Order Detail</h2>
+                                <div class='table-responsive'>
+                                    <table id='myTable' class='table table-striped table-bordered'>
+                                        <thead>
+                                            <tr>
+                                                <th>Order #</th>
+                                                <th>Customer</th>
+                                                <th>Address</th>
+                                                <th>Contact</th>
+                                                <th>Items</th>
+                                                <th>Status</th>
+                                                <th>Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        </tbody>
+                                    </table>
+                                </div>
+               </div>
+</html>
