c87cd9e64d640c8096282a400763c975b01d3990
ahsanahmed324 <158136626+ahsanahmed324@users.noreply.github.com>
2024-04-24 16:58:29 +0500

    Update singleview.php

--- singleview.php
+++ singleview.php
@@ -1,6 +1,6 @@
 <?php
     echo htmlspecialchars($_GET["idd"]);
-    $xx =$GLOBALS['$woocommerce'];
-    $productss = $xx->get('products');
-    print_r($productss);
+    echo $GLOBALS['$woocommerce'];
+    //$productss = $xx->get('products');
+    //print_r($productss);
 ?>
