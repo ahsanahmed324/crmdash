c078125881ab07fe957c5fb12d08d4838bf296a7
ahsanahmed324 <158136626+ahsanahmed324@users.noreply.github.com>
2024-04-26 13:12:45 +0500

    sv

--- singleview.php
+++ singleview.php
@@ -97,10 +97,12 @@
                         echo "</br> "."<span style='font-weight:bold;'> upsell purchase: </span>". $value["name"];
                     }
                     foreach ($ord[meta_data] as $key => $value) {
-                        if ($value["key"] = "_bump_purchase_item_total"){
-                        echo "</br> "."<span style='font-weight:bold;'> bump value= </span>". $value["value"];
+
+                        if ($value["key"] == "_bump_purchase_item_total")
+                        {
+                        echo "</br> "."<span style='font-weight:bold;'> bump value </span>". $value["value"];
                         }
-                        if
+        
                     }
              
              echo"
