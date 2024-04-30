d899784ca3d659eb4e8858c3faa67b99fa8b8b75
ahsanahmed324 <158136626+ahsanahmed324@users.noreply.github.com>
2024-04-30 14:37:44 +0500

    sv

--- singleview.php
+++ singleview.php
@@ -57,7 +57,7 @@
 			    <div class='row' style='border: 1px solid #d1cdcd; border-radius: 12px; padding: 15px; margin-bottom:30px; box-shadow:0px 0px 20px 0px #888888;'>
   			        <div class='col-sm-9'><h2><strong>Order #".$ord['id']."</strong></h2></br>".$ord['id']."  ".$ord["status"]."</br>
 			            <p>". $ord["date_created"] ."</p></br>
-			            <p style='text-decoration: underline; font-size: 16px;'><strong>Address: </strong></br>".$ord["shipping"]["address_1"]." &nbsp;".$ord["shipping"]["address_2"]."</br>". $ord["shipping"]["city"].",".$ord["shipping"]["state"].",".$ord["shipping"]["country"]."</p>
+			            <p style='text-decoration: underline; font-size: 16px;'><strong>Address: </strong></p></br><p>".$ord["shipping"]["address_1"]." &nbsp;".$ord["shipping"]["address_2"]."</br>". $ord["shipping"]["city"].",".$ord["shipping"]["state"].",".$ord["shipping"]["country"]."</p>
                         </br><p style='text-decoration: underline; font-size: 16px;'><strong>Items: </strong></p>";
 			?>
                         <table id='myTable' class='table table-striped table-bordered'>
@@ -100,7 +100,7 @@
             <?php    
             echo"
                     </div>
- 			            <div class='col-sm-3'><h4 style='text-decoration: underline; font-size: 16px;'><strong>Customer detail</strong></h4></br>". $ord["billing"]["first_name"].$ord["billing"]["last_name"]."
+ 			            <div class='col-sm-3'><h4 style='font-size: 16px;'><strong>Customer detail</strong></h4></br>". $ord["billing"]["first_name"].$ord["billing"]["last_name"]."
 			            <p>". $ord["billing"]["email"]."</p>
 			            </br>
 			    </div>
