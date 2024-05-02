661a4d92dc633081f20e242e5a40075c43beee0f
ahsanahmed324 <158136626+ahsanahmed324@users.noreply.github.com>
2024-05-02 12:59:34 +0500

    sv

--- index.php
+++ index.php
@@ -41,8 +41,9 @@ if (isset($_GET['page_no'])){
     $querry = "orders?page=".$currentPage."&"." rel='next'";
 
     try {
-
+        
         $results = $woocommerce->get($querry);
+        echo "page onee";
         //$results = $woocommerce->get('orders');
         $products = $woocommerce->get('products');
         $customers = $woocommerce->get('customers');
