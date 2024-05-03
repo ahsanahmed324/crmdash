6c54a3ff6aeb0bfdd255268fd322303b1d8d07f5
ahsanahmed324 <158136626+ahsanahmed324@users.noreply.github.com>
2024-05-03 17:27:32 +0500

    sv

--- singleview.php
+++ singleview.php
@@ -57,7 +57,7 @@
             echo"<img src='/myherocard-logo-main.webp' style='width:153px; height:102px; padding:20px;'>
                 <div class='container-fluid'>
 			    <div class='row' style='Xborder: 1px solid #d1cdcd; Xborder-radius: 12px; padding: 15px; Xmargin-bottom:30px; Xbox-shadow:0px 0px 20px 0px #888888;'>
-                    <div class='col-md-2' style='col-md-6'>
+                    <div class='col-md-2' style='color: a688fa;'>
                         1 of 3
                     </div>
                     <div class='col-md-7' style='background-color: #282828; Xwidth:70%; Xmargin-right:5px; border-radius: 12px;'><h3 style='color: #ffffff;'><strong>Order #".$ord['id']."</strong></h3></br><span style='color: green; font-weight:bold;'>".$ord["status"]."</span></br>
