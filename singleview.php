d603b64c995f8fde9a057e177d04ff76f1ec6a14
ahsanahmed324 <158136626+ahsanahmed324@users.noreply.github.com>
2024-05-06 14:14:52 +0500

    Update singleview.php

--- singleview.php
+++ singleview.php
@@ -55,7 +55,7 @@
                 <img src='/myherocard-logo-main.webp' style='width:153px; height:102px; padding:20px;'>
                 </div>
                 <div class='container'>
-			                       <div class='col-md-7' style='Xwidth:70%; Xmargin-right:5px; border-radius: 12px;'><h3><strong>Order #".$ord['id']."</strong></h3></br><span style='color: green; font-weight:bold;'>".$ord["status"]."</span></br>
+			                       <div class='col-8' style='Xwidth:70%; Xmargin-right:5px; border-radius: 12px;'><h3><strong>Order #".$ord['id']."</strong></h3></br><span style='color: green; font-weight:bold;'>".$ord["status"]."</span></br>
 			            <p>". $ord["date_created"] ."</p></br>
 			            </br><p style='text-decoration: underline; font-size: 16px;'><strong>Items: </strong></p>";
 			?>
@@ -99,7 +99,7 @@
             <?php    
             echo"
                     </div>
- 			            <div class='col-md-2' style='border-radius:12px; margin-left:5px; Xwidth:25%;'>
+ 			            <div class='col-4' style='border-radius:12px; margin-left:5px; Xwidth:25%;'>
                         <h3 style='color: #ffffff'>Customer Details</h3>
                         <h4 style='font-size: 16px;'><strong>Name & Email</strong></h4><p>". $ord["billing"]["first_name"].$ord["billing"]["last_name"]."</p>
 			            <p style='color: #8f8e8e;'>". $ord["billing"]["email"]."</p>
